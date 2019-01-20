c0824dcbda0afb1b979e763f7b81438c618c11e8
slaterx <slaterx@live.com>
2019-01-20 23:59:54 +1300

    Added two test cases for services, updated .gitignore

--- service/service_internal_test.go
+++ service/service_internal_test.go
@@ -1,9 +1,6 @@
 package service
 
 import (
-	"context"
-	"fmt"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	log "github.com/sirupsen/logrus"
@@ -13,150 +10,15 @@ var logger = log.WithField("Unit Test", true)
 
 var _ = Describe("MergeActions", func() {
 
-	rotateFunc := func(ctx context.Context, params Params) interface{} {
-		return "Hellow Leleu ;) I'm rotating ..."
-	}
-
-	rotatesEventFunc := func(ctx context.Context, params Params) {
-		fmt.Println("spining spining spining")
-	}
-
-	mixinTideFunc := func(ctx context.Context, params Params) interface{} {
-		return "tide influence in the oceans"
-	}
-
-	mixinRotatesFunc := func(ctx context.Context, params Params) {
-		fmt.Println("update tide in relation to the moon")
-	}
-
-	mixinMoonIsCloseFunc := func(ctx context.Context, params Params) {
-		fmt.Println("rise the tide !")
-	}
-
-	serviceSchema := ServiceSchema{
-		Name:    "earth",
-		Version: "0.2",
-		Settings: map[string]interface{}{
-			"dinosauros": true,
-		},
-		Metadata: map[string]interface{}{
-			"star-system": "sun",
-		},
-		Hooks: map[string]interface{}{
-			"solar-system": "true",
-		},
-		Actions: []ServiceActionSchema{
-			ServiceActionSchema{
-				Name:    "rotate",
-				Handler: rotateFunc,
-			},
-		},
-		Events: []ServiceEventSchema{
-			ServiceEventSchema{
-				Name:    "earth.rotates",
-				Handler: rotatesEventFunc,
-			},
-		},
-	}
-
-	moonMixIn := MixinSchema{
-		Name: "moon",
-		Settings: map[string]interface{}{
-			"craters": true,
-		},
-		Metadata: map[string]interface{}{
-			"resolution": "high",
-		},
-		Hooks: map[string]interface{}{
-			"earth": "true",
-		},
-		Actions: []ServiceActionSchema{
-			ServiceActionSchema{
-				Name:    "tide",
-				Handler: mixinTideFunc,
-			},
-		},
-		Events: []ServiceEventSchema{
-			ServiceEventSchema{
-				Name:    "earth.rotates",
-				Handler: mixinRotatesFunc,
-			},
-			ServiceEventSchema{
-				Name:    "moon.isClose",
-				Handler: mixinMoonIsCloseFunc,
-			},
-		},
-	}
+	serviceSchemaOriginal := ServiceSchema{"earth", "0.2", map[string]interface{}(nil), map[string]interface{}(nil), nil, []ServiceAction(nil), []ServiceEvent(nil), nil, nil, nil}
+	serviceSchemaMixin := ServiceSchema{"earth", "0.2", map[string]interface{}(nil), map[string]interface{}(nil), nil, []ServiceAction(nil), []ServiceEvent(nil), nil, nil, nil}
 
 	It("Should merge and overwrite existing actions", func() {
 
-		mergedServiceAction := mergeActions(serviceSchemaOriginal, &serviceSchemaMixin)
+		mergedService := mergeActions(serviceSchemaOriginal, &serviceSchemaMixin)
 
-		Expect(mergedServiceAction.Actions).Should(Equal(serviceSchemaMixin.Actions))
-		Expect(mergedServiceAction.Actions).Should(Not(Equal(serviceSchemaOriginal.Actions)))
-	})
-
-	It("Should merge and overwrite existing events", func() {
-
-		mergedServiceEvent := mergeEvents(serviceSchemaOriginal, &serviceSchemaMixin)
-
-		Expect(mergedService.Actions).Should(Equal([]ServiceActionSchema{
-			ServiceActionSchema{
-				Name:    "rotate",
-				Handler: rotateFunc,
-			},
-			ServiceActionSchema{
-				Name:    "tide",
-				Handler: mixinTideFunc,
-			},
-		}))
-
-	})
-
-	It("Should merge and overwrite existing events", func() {
-
-		mergedService := mergeEvents(serviceSchema, moonMixIn)
-		Expect(mergedService.Events).Should(Equal([]ServiceEventSchema{
-			ServiceEventSchema{
-				Name:    "earth.rotates",
-				Handler: rotatesEventFunc,
-			},
-			ServiceEventSchema{
-				Name:    "moon.isClose",
-				Handler: mixinMoonIsCloseFunc,
-			},
-		},
-		))
-	})
-
-	It("Should merge and overwrite existing settings", func() {
-
-		mergedService := mergeSettings(serviceSchema, moonMixIn)
-		Expect(mergedService.Settings).Should(Equal(map[string]interface{}{
-			"dinosauros": true,
-			"craters":    true,
-		},
-		))
-	})
-
-	It("Should merge and overwrite existing metadata", func() {
-
-		mergedService := mergeMetadata(serviceSchema, moonMixIn)
-		Expect(mergedService.Metadata).Should(Equal(map[string]interface{}{
-			"star-system": "sun",
-			"resolution":  "high",
-		},
-		))
-	})
-
-	It("Should merge and overwrite existing hooks", func() {
-
-		mergedService := mergeHooks(serviceSchema, moonMixIn)
-		Expect(mergedService.Hooks).Should(Equal(map[string]interface{}{
-			"solar-system": "true",
-			"earth":        "true",
-		},
-		))
+		Expect(mergedService.Actions).Should(Equal(serviceSchemaMixin.Actions))
+		Expect(mergedService.Actions).Should(Not(Equal(serviceSchemaOriginal.Actions)))
 	})
 
 })
--- service/service_test.go
+++ service/service_test.go
@@ -1,92 +1,28 @@
 package service_test
 
-//
-//import (
-//	"context"
-//	"fmt"
-//
-//	log "github.com/sirupsen/logrus"
-//
-//	. "github.com/onsi/ginkgo"
-//	. "github.com/onsi/gomega"
-//
-//	. "github.com/moleculer-go/moleculer"
-//)
-//
-//var logger = log.WithField("Unit Test", true)
-//
-//var _ = Describe("MergeActions", func() {
-//
-//	serviceSchema := Service{
-//		Name:    "earth",
-//		Version: "0.2",
-//		Settings: map[string]interface{}{
-//			"dinosauros": true,
-//		},
-//		Metadata: map[string]interface{}{
-//			"star-system": "sun",
-//		},
-//		Actions: []Action{
-//			Action{
-//				Name: "rotate",
-//				Handler: func(ctx context.Context, params Params) interface{} {
-//					return "Hellow Leleu ;) I'm rotating ..."
-//				},
-//			},
-//		},
-//		Events: []Event{
-//			Event{
-//				Name: "earth.rotates",
-//				Handler: func(ctx context.Context, params Params) {
-//					fmt.Println("spining spining spining")
-//				},
-//			},
-//		},
-//	}
-//
-//	moonMixIn := Mixin{
-//		Name: "moon",
-//		Settings: map[string]interface{}{
-//			"craters": true,
-//		},
-//		Metadata: map[string]interface{}{
-//			"resolution": "high",
-//		}, Actions: []Action{
-//			Action{
-//				Name: "tide",
-//				Handler: func(ctx context.Context, params Params) interface{} {
-//					return "tide influence in the oceans"
-//				},
-//			},
-//		},
-//		Events: []Event{
-//			Event{
-//				Name: "earth.rotates",
-//				Handler: func(ctx context.Context, params Params) {
-//					fmt.Println("update tide in relation to the moon")
-//				},
-//			},
-//			Event{
-//				Name: "moon.isClose",
-//				Handler: func(ctx context.Context, params Params) {
-//					fmt.Println("rise the tide !")
-//				},
-//			},
-//		},
-//	}
-//
-//	It("Should merge and overwrite existing actions", func() {
-//
-//		//just to avoid the "not used errors"
-//		Expect(serviceSchema).Should(Not(BeNil()))
-//		Expect(moonMixIn).Should(Not(BeNil()))
-//
-//		// merged := ...
-//
-//		// thisName := serviceSchema.GetName()
-//		// Expect(thisName).Should(Equal(serviceSchemaOriginal.Name))
-//		// Expect(thisName).Should(Not(Equal(serviceSchemaMixin.Name)))
-//
-//	})
-//
-//})
+import (
+	log "github.com/sirupsen/logrus"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/moleculer-go/moleculer/service"
+)
+
+var logger = log.WithField("Unit Test", true)
+
+var _ = Describe("MergeActions", func() {
+
+	serviceSchema := Service{"earth", "0.2", map[string]interface{}(nil), map[string]interface{}(nil), []ServiceAction(nil), []ServiceEvent(nil), nil, nil, nil}
+	serviceSchemaOriginal := ServiceSchema{"earth", "0.2", map[string]interface{}(nil), map[string]interface{}(nil), nil, []ServiceAction(nil), []ServiceEvent(nil), nil, nil, nil}
+	serviceSchemaMixin := ServiceSchema{"venus", "0.2", map[string]interface{}(nil), map[string]interface{}(nil), nil, []ServiceAction(nil), []ServiceEvent(nil), nil, nil, nil}
+
+	It("Should merge and overwrite existing actions", func() {
+
+		thisName := serviceSchema.GetName()
+		Expect(thisName).Should(Equal(serviceSchemaOriginal.Name))
+		Expect(thisName).Should(Not(Equal(serviceSchemaMixin.Name)))
+
+	})
+
+})
