99091d05eac198c622c7c090fec4c2051033720c
Gleidson Nascimento <slaterx@live.com>
2019-02-06 21:06:32 +1300

    Fixing type in round robin

--- strategy/round-robin.go
+++ strategy/round-robin.go
@@ -5,8 +5,12 @@ import (
 	"time"
 )
 
+// RoundRobinStrategy exposes the type as a strategy option
+type RoundRobinStrategy struct {
+}
+
 // SelectTargetNode randomly selects a node out of slice
-func SelectTargetNode(nodes []string) string {
+func (strategy RoundRobinStrategy) SelectTargetNode(nodes []string) string {
 	// We need to seed the rand function. If we use
 	// a static number the sequence is also static. Therefore
 	// using now as unix number.
