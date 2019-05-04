8da7afc7e42562fa0e393d51d2d7914188405cda
Rafael Almeida <rafael@iswe.co.nz>
2019-05-04 15:54:43 +1200

    chore put tests back

--- broker/broker_internals_test.go
+++ broker/broker_internals_test.go
@@ -17,7 +17,7 @@ import (
 
 var _ = Describe("Broker Internals", func() {
 
-	XDescribe("Broker events", func() {
+	Describe("Broker events", func() {
 		eventsTestSize := 1
 		currentStep := 0
 		//TODO needs refactoring.. the test is not realiable and fail from time to time.
