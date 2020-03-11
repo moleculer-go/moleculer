5e14e5401a54298ec6b9bddab0fd14d4a8d2941a
Rafael Almeida <rafael@iswe.co.nz>
2020-03-11 21:27:27 +1300

    fix (service catalog) use action name when updating from remote

--- registry/serviceCatalog.go
+++ registry/serviceCatalog.go
@@ -206,7 +206,7 @@ func (serviceCatalog *ServiceCatalog) updateActions(serviceMap map[string]interf
 		}
 	}
 	for _, action := range current.Actions() {
-		name := action.FullName()
+		name := action.Name()
 		_, exists := actions[name]
 		if !exists {
 			deletedActions = append(deletedActions, action)
